cdfdae80b2141d3d8b972acba68a33757e0b0d40
Richard Remer <reanjr@gmail.com>
2017-01-12 19:42:15 -0800

    CLI usage

---
+++ bin/usage.js
@@ -0,0 +1,12 @@
+/**
+ * Print usage.
+ * @param {string} script
+ */
+function usage() {
+    var usage;
+    
+    usage  = `Usage: ${script} <jiracn> <gitcn> <base> <merge> [<status> ...] 
+    console.log(usage);
+}
+
+module.exports = usage;
