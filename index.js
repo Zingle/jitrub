22744ceccb2460cc1961351e5ea6a2119de944cb
Richard Remer <rich@zingle.me>
2017-01-25 10:36:45 -0800

    export additional types

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 module.exports = require("./lib/jitrub");
+module.exports.github = require("./lib/github");
+module.exports.jira = require("./lib/jira");
+module.exports.creds = require("./lib/creds");
