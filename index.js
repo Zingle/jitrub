2157308ea15793cf61957941c7e73c8dbcd21864
Richard Remer <rich@zingle.me>
2017-01-27 12:12:22 -0800

    throw LockError when branch is already locked

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ module.exports = require("./lib/jitrub");
 module.exports.github = require("./lib/github");
 module.exports.jira = require("./lib/jira");
 module.exports.creds = require("./lib/creds");
+module.exports.LockError = require("./lib/lock-error");
